b195e84cfea6d2d2e6157e1668e4c3fae0f38f13
Juan Michellangelo <jbarreto2105@gmail.com>
2021-02-28 16:51:44 -0300

    Estudando gerenciador de layout "pack" - fill

---
+++ src/006.py
@@ -0,0 +1,18 @@
+from tkinter import*
+
+root = Tk()
+
+lb1 = Label(root, text="escrito 1", bg="purple")
+lb1.pack(side=TOP, fill=X)
+
+lb2 = Label(root, text="escrito 2", bg="pink")
+lb2.pack(side=LEFT, fill=Y)
+
+lb3 = Label(root, text="escrito 3", bg="yellowgreen")
+lb3.pack(side=RIGHT, fill=Y)
+
+lb4 = Label(root, text="escrito 4", bg="green")
+lb4.pack(side=BOTTOM, fill=X)
+
+root.geometry("300x300+100+100")
+root.mainloop()
