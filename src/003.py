9b268b6630c9428c707363da61724940c4bb852b
Juan Michellangelo <jbarreto2105@gmail.com>
2021-02-28 15:55:58 -0300

    Passando escrito do widget de entrada para label, utilizando funcao e parametro command

---
+++ src/003.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from functools import partial
+
+def bt_click(e):
+    lb["text"] = e.get()
+
+root = Tk()
+
+ed = Entry(root)
+ed.place(x=100, y=70)
+
+bt = Button(root, text="Imprimir")
+bt["command"] = partial(bt_click, ed)
+bt.place(x=100, y=100)
+
+lb = Label(root, text="Texto")
+lb.place(x=100, y=130)
+
+root.geometry("300x300+100+100")
+root.mainloop()
