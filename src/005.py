a1b2c331d9c281d16557ccca01d8cbeb0ec43d60
Juan Michellangelo <jbarreto2105@gmail.com>
2021-02-28 16:37:02 -0300

    Estudando gerenciador de layout "pack" - anchor

---
+++ src/005.py
@@ -0,0 +1,17 @@
+from tkinter import*
+
+root = Tk()
+
+lb1 = Label(root, text="Side - 1", bg="orange")
+lb2 = Label(root, text="Side - 2", bg="blue")
+lb3 = Label(root, text="Anchor - 1", bg="blue")
+lb4 = Label(root, text="Anchor - 2", bg="orange")
+
+lb1.pack(side=LEFT)
+lb2.pack(side=LEFT)
+lb3.pack(side=TOP, anchor=E)
+lb4.pack(side=BOTTOM, anchor=E)
+
+
+root.geometry("300x300+100+100")
+root.mainloop()
