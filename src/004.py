f14e4c67411819fa8ddec7e06a5e0f6631d93929
Juan Michellangelo <jbarreto2105@gmail.com>
2021-02-28 16:10:49 -0300

    Estudando gerenciador de layout "pack" - side

---
+++ src/004.py
@@ -0,0 +1,16 @@
+from tkinter import*
+
+root = Tk()
+
+lb1 = Label(root, text="Label 1", bg="lightblue")
+lb2 = Label(root, text="Label 2", bg="lightpink")
+lb3 = Label(root, text="Label 3", bg="lightgreen")
+lb4 = Label(root, text="Label 4", bg="orangered")
+
+lb1.pack()
+lb2.pack(side=LEFT)
+lb3.pack(side=RIGHT)
+lb4.pack(side=BOTTOM)
+
+root.geometry("300x300+100+100")
+root.mainloop()
